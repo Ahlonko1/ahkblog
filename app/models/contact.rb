b3332300a79a0e9e5a681d7ce9f8078bc3736702
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-06-19 22:17:49 +0200

    contact page layout

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < MailForm::Base
-  attribute :first_name,      validate: true
-  attribute :last_name,      validate: true
-  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message
-  attribute :nickname,  captcha: true
+  attribute :first_name, validate: true
+  attribute :last_name, validate: true
+  attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject, validate: true
+  attribute :your_message, validate: true
+  attribute :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
