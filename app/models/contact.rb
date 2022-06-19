756afc0f093d10f7524b1bd221192237557bd4d5
Ahlonko Kowouvi <ahlonko.kowouvi@gmail.com>
2022-06-19 20:37:47 +0200

    contact css

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < MailForm::Base
-  attribute :name,      validate: true
+  attribute :first_name,      validate: true
+  attribute :last_name,      validate: true
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
   attribute :nickname,  captcha: true
